1875ddfc9fe1cb53871feff4dfb0df1d6a169c15
Zeke Mostov <32168567+emostov@users.noreply.github.com>
2020-09-23 21:51:12 -0700

    Update with comments

--- src/txwrapper/handleTypePrefix.ts
+++ src/txwrapper/handleTypePrefix.ts
@@ -2,26 +2,26 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import * as api from '@polkadot/api';
 import * as util from '@polkadot/util';
-import * as txWrapper from '@substrate/txwrapper';
+import * as txwrapper from '@substrate/txwrapper';
 import axios from 'axios';
 
 const url = 'http://localhost:8080';
 
 async function main() {
-	const material = (await axios.get(`${url}/transaction/material/?at=11111`))
+	const material = (await axios.get(`${url}/transaction/material`))
 		.data as Record<string, any>;
 
 	const keyring = new api.Keyring();
 	const alice = keyring.addFromUri('//Alice', { name: 'Alice' }, 'ecdsa');
 
-	const registry = txWrapper.getRegistry(
+	const registry = txwrapper.getRegistry(
 		material.chainName,
 		material.specName,
 		Number(material.specVersion),
 		material.metadata
 	);
 
-	const unsigned = txWrapper.methods.balances.transfer(
+	const unsigned = txwrapper.methods.balances.transfer(
 		{
 			value: '10000',
 			dest: '5DpGPTju2gHJKo8cjWRGwzyUPKHBkGVm7d9HH6XUyn5qi1p6', // random address
@@ -44,7 +44,7 @@ async function main() {
 		}
 	);
 
-	const signingPayload = txWrapper.createSigningPayload(unsigned, {
+	const signingPayload = txwrapper.createSigningPayload(unsigned, {
 		registry,
 	});
 	const extrinsicPayload = registry.createType(
@@ -55,13 +55,20 @@ async function main() {
 		}
 	);
 
-	console.log('test');
-
 	// The below expresion is equilvalent to:
-	// const signature = alice.sign(extrinsicPayload.toU8a({ method: true }), { withType: true });
+	// const
 	const { signature } = extrinsicPayload.sign(alice);
+	// The above line is equivalent to:
+	const exampleSignature = alice.sign(
+		extrinsicPayload.toU8a({ method: true }),
+		{ withType: true }
+	);
+	console.info(
+		'\n`signature` is equivalent to `exampleSignature`: ',
+		util.u8aToHex(exampleSignature) === signature
+	);
 
-	// As it mentioned in the comment within the `ExtrinsincPayload.sign`, the actual payload that is
+	// As mentioned in the comment within the `ExtrinsincPayload.sign`, the actual payload that is
 	// signed is different from `ExtrinsincPayload.toU8a` because it does not have the length of the
 	// `method` property included in the encoding
 	const actualPayloadThatWasSigned = extrinsicPayload.toU8a({ method: true });
@@ -69,8 +76,8 @@ async function main() {
 	// slice out the type prefix that gets concatenated when signing (see links 1 & 2)
 	const signatureWithTypePrefixRemoved = util.hexToU8a(signature).slice(1);
 
-	console.log(
-		'The signature is valid for the payload: ',
+	console.info(
+		'\nThe signature is valid for the payload: ',
 		alice.verify(actualPayloadThatWasSigned, signatureWithTypePrefixRemoved)
 	);
 
